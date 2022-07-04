2cd856af0d62170677d9721ae7b62977ce821e08
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-07-04 10:00:29 -0300

    feat: add strategies to create fares

--- src/Ride.ts
+++ src/Ride.ts
@@ -1,11 +1,11 @@
+import NormalFareCalculator from "./NormalFareCalculator";
+import OvernightFareCalculator from "./OvernightFareCalculator";
+import OvernightSundayFareCalculator from "./OvernightSundayFareCalculator";
 import Segment from "./Segment";
+import SundayFareCalculator from "./SundayFareCalculator";
 
 export default class Ride {
   segments: Segment[] = []
-  NORMAL_FARE = 2.10
-  OVERNIGHT_FARE = 3.90
-  SUNDAY_FARE = 2.9
-  OVERNIGHT_SUNDAY_FARE = 5
   MINIMAL_FARE = 10
 
   constructor() { }
@@ -18,18 +18,22 @@ export default class Ride {
     let fare = 0
     for (const segment of this.segments) {
       if (segment.isOvernight() && !segment.isSunday()) {
-        fare += segment.distance * this.OVERNIGHT_FARE
+        const fareCalculator = new OvernightFareCalculator()
+        fare += fareCalculator.calculate(segment)
         continue
       }
       if (segment.isOvernight() && segment.isSunday()) {
-        fare += segment.distance * this.OVERNIGHT_SUNDAY_FARE
+        const fareCalculator = new OvernightSundayFareCalculator()
+        fare += fareCalculator.calculate(segment)
         continue
       }
       if (segment.isSunday()) {
-        fare += segment.distance * this.SUNDAY_FARE
+        const fareCalculator = new SundayFareCalculator()
+        fare += fareCalculator.calculate(segment)
         continue
       }
-      fare += segment.distance * this.NORMAL_FARE
+      const fareCalculator = new NormalFareCalculator()
+      fare += fareCalculator.calculate(segment)
     }
     return (fare < this.MINIMAL_FARE) ? this.MINIMAL_FARE : fare
   }
